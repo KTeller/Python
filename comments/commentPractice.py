9044368319b55ca7f138db35b1c7514b77ae69c3
KTeller <kteller@wisc.edu>
2021-11-21 17:14:37 -0600

    commentsProject

---
+++ comments/commentPractice.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 21, 2021
+
+@author: ITAUser
+'''
+
+#single line comments
+
+''' 
+multi-line 
+Comments 
+'''
+
+a = 2 #This is two
---
+++ comments/commentsDrill.py
@@ -0,0 +1,23 @@
+'''
+Created on Nov 21, 2021
+
+@author: ITAUser
+'''
+
+#Okay, here's a single line comment
+
+'''
+ oh 
+ you wanted a multi-line comment?
+ well here's that as well 
+ '''
+ 
+a = 3 #here's a variable... and another comment
+ 
+#hey, what's with all the single line comments?
+
+''' 
+This has been a great way to spend my time 
+also has been a tad redundant
+but that's good too
+'''
