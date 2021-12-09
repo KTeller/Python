f96d8908469e2602bd03f05a41b4e251b9155227
KTeller <kteller@wisc.edu>
2021-12-09 17:52:32 -0600

    survey

---
+++ survey/survey.py
@@ -0,0 +1,48 @@
+'''
+Created on Dec 9, 2021
+
+@author: ITAUser
+'''
+
+'''
+The program is a survey that can take information from 
+the user.
+Made by Kamea Teller
+'''
+
+'''
+psudocode
+#Use input() to ask the user "What is your name?" and store that in
+#a variable called name. Then print the variable.
+
+#Use input() to ask the user "What is your favorite color?" and store that 
+#in a variable called color. Then print the variable.
+
+#Use input() to ask the user "What city did you grow up in?" and store 
+#that in a variable called city. Then print the variable.
+
+#Use input() to ask the user "What is your best friends name?" and store 
+#that in a variable called friend. Then print the variable.
+
+#Use input() to ask the user "What color hair do you have?" and store that 
+#in a variable called hair. Then print the variable.
+'''
+name = input("What is your name?")
+print(name)
+
+color = input("What is your favorite color?")
+print(color)
+
+city = input("What city did you grow up in?")
+print(city)
+
+friend = input("What is your best friends name?")
+print(friend)
+
+hair = input("What color hair do you have?")
+print(hair)
+
+
+
+
+
