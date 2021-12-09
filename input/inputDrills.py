ff8c20f44553d282b634209b283e33b4c516b7da
KTeller <kteller@wisc.edu>
2021-12-08 18:41:15 -0600

    input

---
+++ input/inputDrills.py
@@ -0,0 +1,13 @@
+'''
+Created on Dec 8, 2021
+
+@author: ITAUser
+'''
+
+faveAnimal = input("What is your favorite animal?")
+
+favNum = int(input("What is your favorite number?"))
+
+school = input("What is your school's name?")
+
+ans = int(input("What is 2 + 2?"))
